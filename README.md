3a4b94f5aaeb95751a82610e426f57aade75e3d2
jack <jackjack5810@yahoo.com>
2022-03-03 14:27:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rust *wgpu* Graphics Programming: Step-By-Step (4) 
+## Create Point and Line Primitives
+
+This is the source code for fourth video of the YouTube series on step-by-step Rust *wgpu* graphics programming.
+
+This sample *wgpu* app creates point and line primitives. The output from this app is shown in the following image.
+
+![wgpu04](src/images/image01.png)
+
+## Link for All Source Code Used in the Rust *wgpu* Step-By-Step Video Series:
+
+https://github.com/jack1232/wgpu-Step-By-Step
+
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+mod common;
+
+use winit::{
+    event_loop::{EventLoop},
+};
+use std::borrow::Cow;
+
+fn main() {
+    let mut primitive_type = "point-list";
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        primitive_type = &args[1];
+    }
+
+    let mut topology = wgpu::PrimitiveTopology::PointList;
+    let mut index_format = None;
+    if  primitive_type == "line-list" {
+        topology = wgpu::PrimitiveTopology::LineList;
+        index_format = None;
+    } else if  primitive_type == "line-strip" {
+        topology = wgpu::PrimitiveTopology::LineStrip;
+        index_format = Some(wgpu::IndexFormat::Uint32);
+    }
+
+    let inputs = common::Inputs{
+        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+        topology: topology,
+        strip_index_format: index_format
+    };
+    let event_loop = EventLoop::new();    
+    let window = winit::window::Window::new(&event_loop).unwrap(); 
+    
+    window.set_title(&*format!("{}: {}", "Primitive", primitive_type));
+    env_logger::init();   
+    pollster::block_on( common::run(event_loop, window, inputs, 6));    
+}
